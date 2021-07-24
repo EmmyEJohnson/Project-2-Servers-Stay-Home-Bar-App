795b7a35db1fe46a81f00fc7cceeae30dca6b783
EmmyEJohnson <73799846+EmmyEJohnson@users.noreply.github.com>
2021-07-24 16:42:46 -0700

    remove .env

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/flights', {
+mongoose.connect(process.env.DATABASE_URI, {
   useFindAndModify: false,
   useNewUrlParser: true, 
   useCreateIndex: true,
@@ -14,3 +14,16 @@ const db = mongoose.connection;
  db.on('connected', function() {
  	console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
  });
+
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient(process.env.DATABASE_URL, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
---
+++ config/passport.js
@@ -0,0 +1,14 @@
+// CONFIG passport.js
+
+// const { MongoClient } = require('mongodb');
+
+// const client = new MongoClient(process.env.DATABASE_URL, { 
+//   useNewUrlParser: true, 
+//   useUnifiedTopology: true 
+// });
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
