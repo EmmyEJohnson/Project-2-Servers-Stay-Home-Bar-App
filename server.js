4bf3bdb19f8c89bac51d8cc64dd7214399036522
EmmyEJohnson <73799846+EmmyEJohnson@users.noreply.github.com>
2021-07-24 17:38:44 -0700

    passport

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 const methodOverride = require('method-override');
 
 
@@ -20,6 +22,13 @@ app.use(logger('morgan'));
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'BarHopRocks!',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(methodOverride('_method'));
 
 //** App Configuration
