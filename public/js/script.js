26e08024e7dda40fe495d288c382fb20b8ed5abf
EmmyEJohnson <73799846+EmmyEJohnson@users.noreply.github.com>
2021-07-31 09:12:19 -0700

    Create bar videos for bar choices HTML, CSS, JS

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,28 @@
+ // Getting video element using jQuery
+ let video1 = $("#video1");
+  
+ // Check if video is ready to play
+ $(video1).on('canplay', function () {
+     $(video1).mouseenter(function () {
+         $(this).get(0).play();
+     }).mouseleave(function () {
+         $(this).get(0).pause();
+     })
+ });
+
+ // Getting video element using jQuery
+ let video2 = $("#video2");
+
+ // Check if video is ready to play
+ $(video2).on('canplay', function () {
+     $(video2).mouseenter(function () {
+         $(this).get(0).play();
+     }).mouseleave(function () {
+         $(this).get(0).pause();
+     })
+ });
+
+
 // background
 
 
